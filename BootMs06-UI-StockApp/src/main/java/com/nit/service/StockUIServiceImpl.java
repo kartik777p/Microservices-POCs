94808327d706272724d59124fa4c2c74c3cc9280
Neosoft <Neosoft@DESKTOP-AAIK2PJ>
2023-04-21 12:48:53 +0530

    Stock Application UI using JSP and use of webClient communication

---
+++ BootMs06-UI-StockApp/src/main/java/com/nit/service/StockUIServiceImpl.java
@@ -0,0 +1,29 @@
+package com.nit.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+public class StockUIServiceImpl implements IStockUIService {
+	
+	@Override
+	public String fetchTotalPrice(String companyName,int quantity) {
+	//prepare endpoint having res service url
+		String END_POINT="http://localhost:4444/stockPriceCalc/{companyName}/{quantity}";
+		//declare variable at the top for method scope
+		String response=null;
+		//create webClient obj
+		WebClient webClient=WebClient.create();
+		//if company present will get its details
+		try {
+		 response=webClient. get()
+				                 .uri(END_POINT,companyName,quantity)
+				                 .retrieve()
+				                 .bodyToMono(String.class)
+				                 .block();
+		}catch (Exception e) {
+			response="Company Not Found in record";
+		}//catch
+		 return response;
+	}//method
+}//class
