f1d46bcceb33b05d406af08671dd6ad668da578c
Neosoft <Neosoft@DESKTOP-AAIK2PJ>
2023-04-24 16:26:18 +0530

    Working with actuator in spring boot

---
+++ BootMs09-Acutuator-App/src/main/java/com/nit/BootMs09AcutuatorAppApplication.java
@@ -0,0 +1,13 @@
+package com.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMs09AcutuatorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMs09AcutuatorAppApplication.class, args);
+	}
+
+}
