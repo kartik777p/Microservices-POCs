f9e5cff8117a6a5c9b0617539f6a6adc732e9b04
Neosoft <Neosoft@192.168.1.38>
2023-04-26 23:32:35 +0530

    config server client (Issue )

---
+++ BootMs15-Config-Server-Client/src/main/java/com/nit/BootMs15ConfigServerClientApplication.java
@@ -0,0 +1,13 @@
+package com.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMs15ConfigServerClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMs15ConfigServerClientApplication.class, args);
+	}
+
+}
