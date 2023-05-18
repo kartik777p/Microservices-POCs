20f3b6b4fe387085792fbda3a61bac7ed21c075f
Neosoft <Neosoft@DESKTOP-AAIK2PJ>
2023-05-18 17:47:09 +0530

    Apache kafka Consumer impl in spring Boot ms

---
+++ BootMs24-Kafka-Consumer/src/main/java/com/nit/rest/CustomerRestController.java
@@ -0,0 +1,26 @@
+package com.nit.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.condition.ConsumesRequestCondition;
+
+import com.nit.model.Customer;
+import com.nit.service.CustomerService;
+
+@RestController
+public class CustomerRestController {
+
+	@Autowired
+	private CustomerService service;
+	
+	/*@PostMapping(value="/customers")
+	public String saveCustomer(@RequestBody List<Customer> customers) {
+		String returnMsg = service.addCustomerToKafka(customers);
+		return returnMsg;
+	}//saveCustomer  */
+}//saveCustomer
---
+++ BootMs24-Kafka-Consumer/src/main/java/com/nit/service/CustomerService.java
@@ -0,0 +1,40 @@
+package com.nit.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import com.nit.constants.KafkaConstants;
+import com.nit.model.Customer;
+
+@Service
+public class CustomerService {
+	
+	@Autowired
+	private KafkaTemplate<String,Customer> kafkaTemplate;
+
+	/*
+	 This method is used to publish a msg to a kafka topic
+	 */
+	/* public String addCustomerToKafka(List<Customer> customers) {
+		//check list empty or not
+		if(!customers.isEmpty()) {
+			for(Customer cust: customers) {
+				//store msg to kafka topic
+				kafkaTemplate.send(KafkaConstants.TOPIC, cust);
+				System.out.println("************************msg is published to a kafka topic *************");
+			}//for
+		}//if
+		return "Customer records added to kafka Queue Sucessfully...";
+	}//addCustomerToKafka  */
+	
+	@KafkaListener(topics = KafkaConstants.TOPIC, groupId = KafkaConstants.GROUP_ID)
+	public Customer listener(Customer c) {
+		System.out.println("***Msg recieved from Kafka Topic ::" + c);
+		return c;
+	}
+	
+}
