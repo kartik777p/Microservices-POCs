4e5fa19ec1de13e8825f68ca89b4519f7ea8a2cc
Neosoft <Neosoft@DESKTOP-AAIK2PJ>
2023-05-18 17:46:47 +0530

    Apache kafka Producer impl in spring Boot ms

---
+++ BootMs23-Kafka-Producer/src/main/java/com/nit/config/KafkaProducerConfig.java
@@ -0,0 +1,44 @@
+package com.nit.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+
+import com.nit.constants.KafkaConstants;
+import com.nit.model.Customer;
+
+@Configuration
+public class KafkaProducerConfig {
+	
+	/*
+	 * This method is used for DefaultKafkaProducerFactory bean obj
+	 */
+	@Bean
+	public ProducerFactory<String,Customer> getDefaultKafkaProducerFactory(){
+		//Set configurations properties to map obj
+		Map<String,Object> configProperties=new HashMap<>();
+		configProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,KafkaConstants.HOST);
+		configProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,StringSerializer.class);
+		configProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,JsonSerializer.class);
+		//create DeafultProducerFactory  obj having all this details and return
+		return new DefaultKafkaProducerFactory<>(configProperties);	
+	}//getDefaultKafkaProducerFactory
+
+	/*
+	 * This method is used to create  kafkaTemplate bean obj
+	 */
+
+	@Bean(name = "kafkaTemplate")
+	 public KafkaTemplate<String,Customer> getKafkaTemplate(){
+		 return new KafkaTemplate<>(getDefaultKafkaProducerFactory());
+	}//getKafkaTemplate
+}
---
+++ BootMs23-Kafka-Producer/src/test/java/com/nit/BootMs23KafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootMs23KafkaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
